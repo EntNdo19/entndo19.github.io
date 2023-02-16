200ea20de2c72359d642378e158650e7418543a3
EntNdo19 <94893384+EntNdo19@users.noreply.github.com>
2023-02-16 11:05:21 +0100

    Add files via upload

---
+++ myjava3.js
@@ -0,0 +1,8 @@
+//der Prompt Befehl erzeugt ein Eingabefenster.
+// prompt("wie alt bist du?");
+
+
+//Text steht immer unter Anfürungzeichen
+// um Text und Variable zu kombinieren, wird das Pluszeichen verwendet.
+alert("du bist " +(prompt("wie alt bist du?"))+ " Jahre alt");
+if (prompt=1) {"Jahr alt"} else {"Jahre alt"}
